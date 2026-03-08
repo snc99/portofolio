32c40ba52550d92b9f7c41856aa411ad12ffd9d1
Irvan Sandy <irvansandi3@gmail.com>
2026-03-08 16:17:51 +0700

    update dashboard

--- proxy.ts
+++ proxy.ts
@@ -4,7 +4,7 @@ import { jwtVerify } from "jose";
 
 const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const token = request.cookies.get("pw_token")?.value;
   const { pathname, origin } = request.nextUrl;
 
