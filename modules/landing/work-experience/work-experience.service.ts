5e29cfd9ea903650ea91f71cd16c6dd793cdc71c
Irvan Sandy <irvansandi3@gmail.com>
2026-04-12 17:18:06 +0700

    fixing v3

--- modules/landing/work-experience/work-experience.service.ts
+++ modules/landing/work-experience/work-experience.service.ts
@@ -1,8 +1,13 @@
 import { prisma } from "@/infrastructure/database/prisma";
 import { WorkExperienceData } from "./work-experience.type";
 
-export async function getWorkExperiences(): Promise<WorkExperienceData[]> {
+export async function getWorkExperiences(
+  page: number = 1,
+  limit: number = 5,
+): Promise<WorkExperienceData[]> {
   return prisma.workExperience.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
     orderBy: {
       startDate: "desc",
     },
