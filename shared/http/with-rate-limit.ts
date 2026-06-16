c6fe245da765ccf48394016d03056486254f7a2d
Irvan Sandy <irvansandi3@gmail.com>
2026-06-16 16:09:30 +0700

    16 Juni 2026 fixing prod

--- shared/http/with-rate-limit.ts
+++ shared/http/with-rate-limit.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 import { ApiResponse } from "@/shared/response/api-response.util";
 import { LRUCache } from "lru-cache";
 
-// const FREEZE_DURATION = 1000 * 60 * 30; // 30 menit
-const FREEZE_DURATION = 10_000; // 10 detik untuk testing
+const FREEZE_DURATION = 1000 * 60 * 30; // 30 menit
+// const FREEZE_DURATION = 10_000; // 10 detik untuk testing
 
 const cache = new LRUCache<string, number>({
   max: 500,
